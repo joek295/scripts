3d8a30346e342534c97bf4d87b15003d2a3f2936
joek295 <joe.kitchen@yahoo.co.uk>
2018-10-25 20:40:54 +0100

    new file:   fclean.py - filename cleaner implemented in python

---
+++ fclean.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# TODO: implement --recursive, --verbose
+
+import getopt
+import os
+import pprint
+import re
+import sys
+
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'hrv', ['help',
+                                                             'recursive',
+                                                             'verbose'])
+
+HELPTEXT = '''usage: fclean [arguments] [folder]	clean files in specified folder
+   or: fclean [arguments]		clean files in current working directory
+
+Arguments:
+  -h or --help:		Print help (this message) and exit
+  -r or --recursive:	Also clean files in subfolders of specified folder
+  -v or --verbose:	Make fclean be verbose, showing files as they are renamed'''
+
+for opt, arg in options:
+    if opt in ('-h', '--help'):
+        print(HELPTEXT)
+        break
+    elif opt in ('-r', '--recursive'):
+        recursive = True
+    elif opt in ('-v', '--verbose'):
+        verbose = True
+
+def get_files(folder, recursive):
+    files = []
+    if recursive is True:
+        for root, directories, filenames in os.walk(folder):
+            for directory in directories:
+                files.append(os.path.join(root, directory))
+            for filename in filenames:
+                files.append(os.path.join(root, filename))
+    else:
+        files = os.listdir(folder)
+    return files
+
+def clean_filenames(files):
+    transformations = {}
+    for file in files:
+        output = re.sub("\s+", "_", file)
+        output = re.sub("[^\w\-\./]", "", output)
+        output = str.lower(output)
+        transformations[file] = output
+    return transformations
+
+def check_filenames(fname_dict):
+    return len(fname_dict) != len(set(fname_dict.values()))
+
+transform_dict = clean_filenames(get_files(".", False))
+if not check_filenames(transform_dict):
+    for filename in transform_dict:
+        os.rename(filename, transform_dict[filename])
+
